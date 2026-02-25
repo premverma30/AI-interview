e9a17e1e6b4a1284a650d3230fa92607ef4c73de
Prem Verma <premv9546@gmail.com>
2026-02-25 16:28:58 +0530

    Added client and models folder

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is my React app.</p>
+    </div>
+  );
+}
+
+export default App;
